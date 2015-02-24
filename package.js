4d46edfb74980e4c2de00d38a7593117dfa0e8f7
Pusher Robot <jack@semantic-ui.com>
2015-02-24 12:10:14 -0500

    Updated component to version 1.10.1

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-header',
   summary : 'Semantic UI - Header: Single component release',
-  version : '1.10.0',
+  version : '1.10.1',
   git     : 'git://github.com/Semantic-Org/UI-Header.git',
 });
 
