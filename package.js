d8201435e36cb2f35e21b3c8888cede4b3caed23
Jack Lukic <jack@semantic-ui.com>
2017-02-07 10:56:54 -0500

    Updated component to version 2.2.7

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-header',
   summary : 'Semantic UI - Header: Single component release',
-  version : '2.2.6',
+  version : '2.2.7',
   git     : 'git://github.com/Semantic-Org/UI-Header.git',
 });
 
